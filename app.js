bb4d1cf7db482363236a4f8b90cc3c85827d8e40
Nomi-Arain20 <naumanarain20@gmail.com>
2024-10-09 13:53:44 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+// alert("hello world")
+// var fname="noman arain";
+// console.log(fname);
+// prompt("what is your name?")
+
+// var values=5;
+// var values2=7;
+
+// console.log(values+values2);
+
+
+
+// var greeting ="hello world";
+// console.log(greeting);
+// var num =5;
+// num -= 4;
+// var num2 = 7;
+// alert(num + num2)
+
+// var prp = parseInt (prompt("what is your name !"));
+// prp = parseInt(prp);
+// console.log(prp + 5);
+// var num = 5;
+// var num2 = 5;
+// console.log(num!==num2);
+// var prp = parseInt (prompt("what is your name !"));
+// if(prp % 3 === 0){
+// console.log("this is divisble by 3");
+// }  else if (prp === 4) {
+//     console.log("fav number");
+// }
+// else if(prp===5) {
+//     console.log("manhoos number");
+// }
+
+//  else{
+//     console.log("this is not divisble by 3!");
+// }
+
+// if(num % 3===0 && num % 5===0){
+//     console.log(true);
+// } else{
+//     console.log(false);
+// }
+// var prp = parseInt (prompt("what is your name !"));
+// if(prp  >= 80 ){console.log( "A+");};
+
+
